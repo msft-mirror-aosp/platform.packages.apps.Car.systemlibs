8de7fd1874b6611bd6ae91147de655fa5f469453
Jane Ha <hatrang@google.com>
2025-03-14 23:39:41 +0000

    Refactor DataSubscriptionController for reusability
    Bug: 392944803
    Test: atest DataSubscriptionControllerTest
    Flag: EXEMPT refactor
    
    Change-Id: I1c4011c1e59783d59b6489a3e24ba7428a3efb9b

---
+++ car-data-subscription-lib/src/com/android/car/datasubscription/DataSubscriptionController.java
@@ -0,0 +1,499 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.car.datasubscription;
+
+import static android.Manifest.permission.ACCESS_NETWORK_STATE;
+import static android.Manifest.permission.INTERNET;
+
+import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.app.ActivityTaskManager;
+import android.app.TaskStackListener;
+import android.car.drivingstate.CarUxRestrictions;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
+import android.net.ConnectivityManager;
+import android.net.Network;
+import android.net.NetworkCapabilities;
+import android.os.Build;
+import android.os.Handler;
+import android.os.Looper;
+import android.text.TextUtils;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.VisibleForTesting;
+
+import com.android.car.ui.utils.CarUxRestrictionsUtil;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Controller to display the data subscription messages
+ */
+public class DataSubscriptionController implements DataSubscription.DataSubscriptionChangeListener,
+        DataSubscriptionViewActionListener {
+    private static final boolean DEBUG = Build.IS_DEBUGGABLE;
+    private static final String TAG = DataSubscriptionController.class.toString();
+    private static final String DATA_SUBSCRIPTION_ACTION =
+            "android.intent.action.DATA_SUBSCRIPTION";
+    private static final String DATA_SUBSCRIPTION_SHARED_PREFERENCE_PATH =
+            "com.android.car.systemui.car.qc.DataSubscriptionController";
+    // Timeout for network callback in ms
+    private static final int CALLBACK_TIMEOUT_MS = 1000;
+    private final Context mContext;
+    private DataSubscription mSubscription;
+    private final Intent mIntent;
+    private ConnectivityManager mConnectivityManager;
+    private DataSubscriptionNetworkCallback mNetworkCallback;
+    private final Handler mMainHandler;
+    private Executor mBackgroundExecutor;
+    private Set<String> mActivitiesBlocklist;
+    private Set<String> mPackagesBlocklist;
+    private CountDownLatch mLatch;
+    private boolean mIsNetworkCallbackRegistered;
+    private NetworkTaskEventListener mNetworkTaskEventListener;
+    private int mUserId = -1;
+    private final TaskStackListener mTaskStackListener = new TaskStackListener() {
+        @SuppressLint("MissingPermission")
+        @Override
+        public void onTaskMovedToFront(ActivityManager.RunningTaskInfo taskInfo) {
+            if (mIsNetworkCallbackRegistered && mConnectivityManager != null) {
+                mConnectivityManager.unregisterNetworkCallback(mNetworkCallback);
+                mIsNetworkCallbackRegistered = false;
+            }
+            if (taskInfo.topActivity == null || mConnectivityManager == null) {
+                return;
+            }
+
+            mTopPackage = taskInfo.topActivity.getPackageName();
+
+            if (mPackagesBlocklist.contains(mTopPackage)) {
+                return;
+            }
+
+            mTopActivity = taskInfo.topActivity.flattenToString();
+
+            if (mActivitiesBlocklist.contains(mTopActivity)) {
+                return;
+            }
+
+            PackageInfo packageInfo;
+            ApplicationInfo appInfo;
+            try {
+                if (mUserId != -1) {
+                    packageInfo = mContext.getPackageManager().getPackageInfoAsUser(mTopPackage,
+                            PackageManager.GET_PERMISSIONS, mUserId);
+                    appInfo = mContext.getPackageManager().getApplicationInfoAsUser(
+                            mTopPackage, 0, mUserId);
+                } else {
+                    packageInfo = mContext.getPackageManager().getPackageInfo(mTopPackage,
+                            PackageManager.GET_PERMISSIONS);
+                    appInfo = mContext.getPackageManager().getApplicationInfo(
+                            mTopPackage, 0);
+                }
+                if (packageInfo != null) {
+                    String[] permissions = packageInfo.requestedPermissions;
+                    boolean appReqInternet = Arrays.asList(permissions).contains(
+                            ACCESS_NETWORK_STATE)
+                            && Arrays.asList(permissions).contains(INTERNET);
+                    if (!appReqInternet) {
+                        mActivitiesBlocklist.add(mTopActivity);
+                        return;
+                    }
+                }
+
+                mTopLabel = appInfo.loadLabel(mContext.getPackageManager());
+                int uid = appInfo.uid;
+                mConnectivityManager.registerDefaultNetworkCallbackForUid(uid, mNetworkCallback,
+                        mMainHandler);
+                mIsNetworkCallbackRegistered = true;
+                // since we don't have the option of using the synchronous call of getting the
+                // default network by UID, we need to set a timeout period to make sure the network
+                // from the callback is updated correctly before deciding to display the message
+                //TODO: b/336869328 use the synchronous call to update network status
+                mLatch = new CountDownLatch(CALLBACK_TIMEOUT_MS);
+                mBackgroundExecutor.execute(() -> {
+                    try {
+                        mLatch.await(CALLBACK_TIMEOUT_MS, TimeUnit.MILLISECONDS);
+                    } catch (InterruptedException e) {
+                        Log.e(TAG, "error updating network callback" + e);
+                    } finally {
+                        if (mNetworkCallback.mNetwork == null) {
+                            mNetworkCapabilities = null;
+                            updateShouldDisplayReactiveMsgForApp(mTopLabel);
+                        }
+                    }
+                });
+            } catch (Exception e) {
+                Log.e(TAG, mTopPackage + " not found : " + e);
+            }
+        }
+    };
+
+    private final CarUxRestrictionsUtil.OnUxRestrictionsChangedListener
+            mUxRestrictionsChangedListener =
+            new CarUxRestrictionsUtil.OnUxRestrictionsChangedListener() {
+                @Override
+                public void onRestrictionsChanged(@NonNull CarUxRestrictions carUxRestrictions) {
+                    mIsDistractionOptimizationRequired =
+                            carUxRestrictions.isRequiresDistractionOptimization();
+                    if (mNetworkTaskEventListener != null) {
+                        mNetworkTaskEventListener.onUxrChange(mIsDistractionOptimizationRequired);
+                    }
+                }
+            };
+
+    // Determines whether a proactive message was already displayed
+    private boolean mWasProactiveMsgDisplayed;
+    private boolean mIsDistractionOptimizationRequired;
+    private boolean mShouldDisplayProactiveMsg;
+
+    private boolean mShouldDisplayReactiveMsg;
+    private String mTopActivity;
+    private String mTopPackage;
+    private CharSequence mTopLabel;
+    private NetworkCapabilities mNetworkCapabilities;
+    private boolean mIsUxRestrictionsListenerRegistered;
+    private SharedPreferences mSharedPreferences;
+    private SharedPreferences.Editor mEditor;
+    private int mCurrentInterval;
+    private int mCurrentCycle;
+    private int mCurrentActiveDays;
+
+    @VisibleForTesting
+    static final String KEY_PREV_POPUP_DATE =
+            "com.android.car.systemui.car.qc.PREV_DATE";
+    @VisibleForTesting
+    static final String KEY_PREV_POPUP_CYCLE =
+            "com.android.car.systemui.car.qc.PREV_CYCLE";
+    @VisibleForTesting
+    static final String KEY_PREV_POPUP_ACTIVE_DAYS =
+            "com.android.car.systemui.car.qc.PREV_ACTIVE_DAYS";
+    @VisibleForTesting
+    static final String KEY_PREV_POPUP_STATUS =
+            "com.android.car.systemui.car.qc.PREV_STATUS";
+
+    @SuppressLint("MissingPermission")
+    public DataSubscriptionController(Context context) {
+        mContext = context;
+        mSubscription = new DataSubscription(context);
+        mMainHandler = new Handler(Looper.getMainLooper());
+        mBackgroundExecutor = Executors.newSingleThreadScheduledExecutor();
+        mIntent = new Intent(DATA_SUBSCRIPTION_ACTION);
+        mIntent.setPackage(mContext.getString(
+                R.string.connectivity_flow_app));
+        mIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        mConnectivityManager = mContext.getSystemService(ConnectivityManager.class);
+        mNetworkCallback = new DataSubscriptionNetworkCallback();
+        mActivitiesBlocklist = new HashSet<>();
+        mPackagesBlocklist = new HashSet<>();
+
+        Resources res = mContext.getResources();
+        String[] blockActivities = res.getStringArray(
+                R.array.config_dataSubscriptionBlockedActivitiesList);
+        mActivitiesBlocklist.addAll(List.of(blockActivities));
+        String[] blockComponents = res.getStringArray(
+                R.array.config_dataSubscriptionBlockedPackagesList);
+        mPackagesBlocklist.addAll(List.of(blockComponents));
+        mSharedPreferences = mContext.getSharedPreferences(
+                DATA_SUBSCRIPTION_SHARED_PREFERENCE_PATH, Context.MODE_PRIVATE);
+        mEditor = mSharedPreferences.edit();
+    }
+
+    void updateShouldDisplayProactiveMsg() {
+        mShouldDisplayProactiveMsg = !mWasProactiveMsgDisplayed
+                && mSubscription.isDataSubscriptionInactive()
+                && isValidTimeInterval()
+                && isValidCycle()
+                && isValidActiveDays();
+        if (mNetworkTaskEventListener.onDataSubscriptionStatusChanged(
+                mIsDistractionOptimizationRequired,
+                mShouldDisplayProactiveMsg)) {
+            writeLatestPopupDate();
+            writeLatestPopupCycle();
+            writeLatestPopupActiveDays();
+        }
+    }
+
+    private void updateShouldDisplayReactiveMsgForApp(CharSequence appLabel) {
+        mShouldDisplayReactiveMsg = ((mNetworkCapabilities == null
+                || (!isSuspendedNetwork() && !isValidNetwork()))
+                && mSubscription.isDataSubscriptionInactive());
+        mNetworkTaskEventListener.onAppForeground(mIsDistractionOptimizationRequired,
+                mShouldDisplayReactiveMsg, appLabel);
+        mActivitiesBlocklist.add(mTopActivity);
+    }
+
+    /** Register needed listeners */
+    @Override
+    public void registerListeners() {
+        mSubscription.addDataSubscriptionListener(this);
+        try {
+            ActivityTaskManager.getService().registerTaskStackListener(mTaskStackListener);
+        } catch (Exception e) {
+            Log.e(TAG, "error while registering TaskStackListener " + e);
+        }
+        if (!mIsUxRestrictionsListenerRegistered) {
+            CarUxRestrictionsUtil.getInstance(mContext).register(
+                    mUxRestrictionsChangedListener);
+            mIsUxRestrictionsListenerRegistered = true;
+        }
+        updateCurrentInterval();
+        updateCurrentCycle();
+        updateCurrentActiveDays();
+        updateCurrentStatus();
+        updateShouldDisplayProactiveMsg();
+    }
+
+    /** Unregister active listeners */
+    @Override
+    public void unregisterListeners() {
+        mSubscription.removeDataSubscriptionListener();
+        if (mIsUxRestrictionsListenerRegistered) {
+            CarUxRestrictionsUtil.getInstance(mContext).unregister(
+                    mUxRestrictionsChangedListener);
+            mIsUxRestrictionsListenerRegistered = false;
+        }
+    }
+
+    @Override
+    public void setNetworkTaskEventListener(NetworkTaskEventListener networkTaskEventListener) {
+        mNetworkTaskEventListener = networkTaskEventListener;
+    }
+
+    boolean isValidNetwork() {
+        return mNetworkCapabilities.hasCapability(
+                NetworkCapabilities.NET_CAPABILITY_VALIDATED);
+    }
+
+    boolean isSuspendedNetwork() {
+        return !mNetworkCapabilities.hasCapability(
+                NetworkCapabilities.NET_CAPABILITY_NOT_SUSPENDED);
+    }
+
+    @Override
+    public void onChange(int value) {
+        updateCurrentStatus();
+        updateShouldDisplayProactiveMsg();
+    }
+
+    @Override
+    public void onMsgDismissed() {
+        if (!mWasProactiveMsgDisplayed) {
+            mWasProactiveMsgDisplayed = true;
+        }
+    }
+
+    public class DataSubscriptionNetworkCallback extends ConnectivityManager.NetworkCallback {
+        Network mNetwork;
+
+        @Override
+        public void onAvailable(@NonNull Network network) {
+            if (DEBUG) {
+                Log.d(TAG, "onAvailable " + network);
+            }
+            mNetwork = network;
+            mLatch.countDown();
+        }
+
+        @Override
+        public void onCapabilitiesChanged(@NonNull Network network,
+                @NonNull NetworkCapabilities networkCapabilities) {
+            if (DEBUG) {
+                Log.d(TAG, "onCapabilitiesChanged " + network);
+            }
+            mNetwork = network;
+            mNetworkCapabilities = networkCapabilities;
+            updateShouldDisplayReactiveMsgForApp(mTopLabel);
+        }
+    }
+
+    public void setUserId(int userId) {
+        mUserId = userId;
+    }
+
+    private boolean isValidTimeInterval() {
+        return mCurrentInterval >= mContext.getResources().getInteger(
+                R.integer.data_subscription_pop_up_frequency);
+    }
+
+    private boolean isValidCycle() {
+        if (mCurrentCycle == 1) {
+            return true;
+        }
+        return mCurrentCycle <= mContext.getResources().getInteger(
+                R.integer.data_subscription_pop_up_startup_cycle_limit);
+    }
+
+    private boolean isValidActiveDays() {
+        if (mCurrentActiveDays == 1) {
+            return true;
+        }
+        return mCurrentActiveDays <= mContext.getResources().getInteger(
+                R.integer.data_subscription_pop_up_active_days_limit);
+    }
+
+    @VisibleForTesting
+    void updateCurrentStatus() {
+        int prevStatus = mSharedPreferences.getInt(KEY_PREV_POPUP_STATUS, 0);
+        int currentStatus = mSubscription.getDataSubscriptionStatus();
+        if (prevStatus == DataSubscriptionStatus.INACTIVE && prevStatus != currentStatus) {
+            mEditor.clear();
+            mEditor.apply();
+        }
+        mEditor.putInt(KEY_PREV_POPUP_STATUS, currentStatus);
+        mEditor.apply();
+    }
+
+    private void updateCurrentInterval() {
+        mCurrentInterval = mContext.getResources().getInteger(
+                R.integer.data_subscription_pop_up_frequency);
+        String prevDate = mSharedPreferences.getString(KEY_PREV_POPUP_DATE, /* defValue=*/ "");
+        if (!TextUtils.isEmpty(prevDate)) {
+            mCurrentInterval = (int) ChronoUnit.DAYS.between(LocalDate.parse(prevDate),
+                    LocalDate.now(ZoneId.systemDefault()));
+        }
+    }
+
+    private void updateCurrentCycle() {
+        mCurrentCycle = mSharedPreferences.getInt(
+                KEY_PREV_POPUP_CYCLE, /* defValue=*/ 0);
+    }
+
+    private void updateCurrentActiveDays() {
+        mCurrentActiveDays = mSharedPreferences.getInt(
+                KEY_PREV_POPUP_ACTIVE_DAYS, /* defValue=*/ 0);
+    }
+
+    private void writeLatestPopupDate() {
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern(mContext.getString(
+                R.string.config_latestPopUpDisplayDateTimeFormat));
+        LocalDate newDate = LocalDate.now(ZoneId.systemDefault());
+        String formattedNewDate = newDate.format(formatter);
+        mEditor.putString(KEY_PREV_POPUP_DATE, formattedNewDate);
+        mEditor.apply();
+    }
+
+    private void writeLatestPopupCycle() {
+        mEditor.putInt(KEY_PREV_POPUP_CYCLE, mSharedPreferences.getInt(
+                KEY_PREV_POPUP_CYCLE, /* defValue=*/ 1) + 1);
+        mEditor.apply();
+    }
+
+    private void writeLatestPopupActiveDays() {
+        mEditor.putInt(KEY_PREV_POPUP_ACTIVE_DAYS, mSharedPreferences.getInt(
+                KEY_PREV_POPUP_ACTIVE_DAYS, /* defValue=*/ 1) + 1);
+        mEditor.apply();
+    }
+
+    @VisibleForTesting
+    void setSubscription(DataSubscription dataSubscription) {
+        mSubscription = dataSubscription;
+    }
+
+    @VisibleForTesting
+    boolean getShouldDisplayProactiveMsg() {
+        return mShouldDisplayProactiveMsg;
+    }
+
+    @VisibleForTesting
+    void setPackagesBlocklist(Set<String> list) {
+        mPackagesBlocklist = list;
+    }
+
+    @VisibleForTesting
+    void setActivitiesBlocklist(Set<String> list) {
+        mActivitiesBlocklist = list;
+    }
+
+    @VisibleForTesting
+    void setConnectivityManager(ConnectivityManager connectivityManager) {
+        mConnectivityManager = connectivityManager;
+    }
+
+    @VisibleForTesting
+    TaskStackListener getTaskStackListener() {
+        return mTaskStackListener;
+    }
+
+    @VisibleForTesting
+    boolean getShouldDisplayReactiveMsg() {
+        return mShouldDisplayReactiveMsg;
+    }
+
+    @VisibleForTesting
+    void setNetworkCallback(DataSubscriptionNetworkCallback callback) {
+        mNetworkCallback = callback;
+    }
+
+    @VisibleForTesting
+    void setIsCallbackRegistered(boolean value) {
+        mIsNetworkCallbackRegistered = value;
+    }
+
+    @VisibleForTesting
+    void setIsUxRestrictionsListenerRegistered(boolean value) {
+        mIsUxRestrictionsListenerRegistered = value;
+    }
+
+    @VisibleForTesting
+    void setSharedPreference(SharedPreferences sharedPreference) {
+        mSharedPreferences = sharedPreference;
+    }
+
+    @VisibleForTesting
+    void setCurrentInterval(int currentInterval) {
+        mCurrentInterval = currentInterval;
+    }
+
+    @VisibleForTesting
+    void setCurrentCycle(int cycle) {
+        mCurrentCycle = cycle;
+    }
+
+    @VisibleForTesting
+    void setCurrentActiveDays(int activeDays) {
+        mCurrentActiveDays = activeDays;
+    }
+
+    @VisibleForTesting
+    void setWasProactiveMsgDisplayed(boolean value) {
+        mWasProactiveMsgDisplayed = value;
+    }
+
+    @VisibleForTesting
+    void setEditor(SharedPreferences.Editor editor) {
+        mEditor = editor;
+    }
+}
