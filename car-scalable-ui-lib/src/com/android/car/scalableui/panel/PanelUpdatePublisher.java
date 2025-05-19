d5970852cdd2cc76dda91c0011002cbe37498fa3
Ankit Shetgaonkar <ankiit@google.com>
2025-05-19 09:51:26 -0700

    Add PanelUpdatePublisher
     - Adds a new interface which defines all the params the Panel can
       expose for updates.
     - Currently we are only exposing Bounds. But we may be add more params
       as required.
    Bug: 418774881
    Test: Manual
    Flag: com.android.car.scalableui.enable_ext_panel_updates
    
    Change-Id: Id4e68c8f1dfcb826a175dcf1bfb0963d93c5caea
    (cherry picked from commit 2823a3885440535bacb49b40e14d9d6d31dce96c)

---
+++ car-scalable-ui-lib/src/com/android/car/scalableui/panel/PanelUpdatePublisher.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2025 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.android.car.scalableui.panel;
+
+import android.graphics.Rect;
+
+/**
+ * Interface for components that can publish updates about a panel's state.
+ *
+ * <p>This publisher is used by ScalableUI constructs, like {@link Panel}, to announce changes
+ * that components outside of ScalableUI (e.g., SystemUI windows) might be interested in.
+ */
+public interface PanelUpdatePublisher {
+    /**
+     * Posts an update about a panel's bounds.
+     *
+     * <p>This method is called when a panel's dimensions or position have changed,
+     * allowing interested observers to react to the new bounds.
+     *
+     * @param panelId The unique identifier of the panel whose bounds have changed.
+     * @param bounds  A {@link android.graphics.Rect} object representing the new bounds of the
+     *                panel.
+     */
+    void postBounds(String panelId, Rect bounds);
+}
